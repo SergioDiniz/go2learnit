5dcbd36a3b5ad42ef691fef04f1a6695c57bfb5c
Sergio Diniz <sergiodinizsh@gmail.com>
2016-06-28 20:34:46 -0300

    user login

--- go2learnit/app/controllers/home_controller.rb
+++ go2learnit/app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :show_home?, only: [:index]
+
   def index
   end
+
+  def show_home?
+    if user_signed_in?
+      redirect_to user_path
+    end
+  end
+
 end
--- go2learnit/config/routes.rb
+++ go2learnit/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
+
+  resources :users, only: [:index]
+
+  get '/user', to: 'users#index', as: 'user'
+
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
