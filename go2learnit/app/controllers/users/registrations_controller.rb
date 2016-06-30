f35a340dd5bfc0249655a2ac58a4010ef2f399c1
Sergio Diniz <sergiodinizsh@gmail.com>
2016-06-30 08:38:57 -0300

    new font to logo, problem with bootstrap js and turbolink resolved

--- go2learnit/app/controllers/users/registrations_controller.rb
+++ go2learnit/app/controllers/users/registrations_controller.rb
@@ -46,7 +46,7 @@ before_action :authenticate_user!, except: [:new, :create]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :last_name, :username, :avatar, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :avatar, :bio])
   end
 
   # The path used after sign up.
